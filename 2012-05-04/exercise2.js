3bd33bf676e6e919e13f6ccd0f6bf98066893f43
Giuseppe Zappavigna <giu.89_@hotmail.it>
2012-05-05 18:08:37 +0200

    esercizi Homework

--- 2012-05-04/exercise2.js
+++ 2012-05-04/exercise2.js
@@ -50,6 +50,25 @@ var control1 = control.map(function(p) {return [p[0]-0.2,p[1]-0.3,p[2]]});
 var ruota1=T([0,1,2])([2.2,-1.2,0.3])(ruota);
 var ruota2=T([0,1,2])([2.2,-1.2,1.2])(ruota);
 var rEs=COLOR([0,0,0])(STRUCT([sostegni,ruota1,ruota2]));
-var fuso=COLOR([0,128/255,0])(STRUCT([piece1,piece,retroMapped]))
-var fusoliera=STRUCT([fuso,rEs,s]);
+var fuso=COLOR([0,128/255,0])(STRUCT([piece1,piece,retroMapped]));
+var pp=[[0,0,0],[0,0.5,0.2],[0,1,0]];
+var pp1=[[0,0,0],[0,0.5,-0.2],[0,1,0]];
+var pp2=[[0,0,0],[0,-0.5,0.2],[0,-1,0]];
+var pp3=[[0,0,0],[0,-0.5,-0.2],[0,-1,0]];
+var cc = BEZIER(S0)(pp);
+  var cc1 = BEZIER(S0)(pp1);
+  var cc2 = BEZIER(S0)(pp2);
+  var cc3 = BEZIER(S0)(pp3);
+    var dominio = INTERVALS(1)(30);
+var domain2 = DOMAIN([[0,1],[0,1]])([20,30]);
+var elica =  CUBIC_HERMITE(S1)([cc,cc1,[0.2,0,0],[-0.2,0,0]]);
+   var elica1 =  CUBIC_HERMITE(S1)([cc,cc1,[-0.2,0,0],[0.2,0,0]]);
+    elica = MAP(elica)(domain2);
+   elica1 = MAP(elica1)(domain2);
+
+  var eliche=STRUCT([elica,elica1]);
+  var elica2=T([1])([-1.2])(eliche);
+  var eliche1=COLOR([139/255,69/255,19/255])(STRUCT([eliche,elica2]));
+  eliche1= T([0,1,2])([3.25,0.5,0.7])(eliche1);
+var fusoliera=STRUCT([fuso,rEs,s,eliche1]);
 DRAW(fusoliera);
